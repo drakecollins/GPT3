967acf131f934ce9e0d4dcc8eaad7b9dc4edb65e
Drake Collins <drakecollins95@outlook.com>
2022-02-13 15:36:31 -0700

    GPT3-Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
+import './navbar.css';
+
+// BEM => Block Element Modifier
+
+const Navbar = () => {
+    return (
+        <div className='gpt3__navbar'>
+            <div className='gpt3__navebar-links'>
+                <div className='gpt3__navbar-links_logo'>
+                    <img src={logo} alt='logo' />
+                </div>
+                <div classNAme='gpt3__navbar-links_container'>
+                    <p><a href='#home'>Home</a></p>
+                    <p><a href='#wgpt3'>What is GPT3?</a></p>
+                    <p><a href='#possibility'>Open AI</a></p>
+                    <p><a href='#features'>Case Studies</a></p>
+                    <p><a href='#blog'>Library</a></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
